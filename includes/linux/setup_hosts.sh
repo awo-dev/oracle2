597ecdfa216ea0ae93185b14f619ef9e0b09b9bc
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 11:53:12 +0200

    adding ./

--- includes/linux/setup_hosts.sh
+++ includes/linux/setup_hosts.sh
@@ -14,7 +14,7 @@ externalIP=$3;
 # Add FQDN to the external IP           : sed "/^#10.174.234.99/ s/$/ $1/" /etc/hosts
 # Change the external IP address        : sed "s/10.174.234.99/10.174.234.$2/" /etc/hosts
 
-sed -e "/sbsv12l6master/c $externalIP\t$fqdn\t$subdomain" /etc/hosts > new_hosts.txt
+sed -e "/sbsv12l6master/c $externalIP\t$fqdn\t$subdomain" /etc/hosts > ./new_hosts.txt
 #sed -e "s/\S*\(sbsv12l6master\)\S*//g" -e "/^#127.0.0.1/ s/$/ $fqdn/" -e "/^#10.174.234.98/ s/$/ $fqdn/" -e "s/10.174.234.98/$externalIP/" /etc/hosts > ./new_hosts.txt
 #mv /etc/hosts /etc/hosts.original
 #mv ./new_hosts.txt /etc/hosts
