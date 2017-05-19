836353ce70e9ca64756c37f7df9e49d9c513a7c0
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 13:01:17 +0200

    testing

--- includes/linux/vhosts.sh
+++ includes/linux/vhosts.sh
@@ -3,5 +3,5 @@ fqdn=$1;
 
 sed "s/sbsv12l6master.softbase.dk/$fqdn/" /home/oracle/Oracle/Middleware12c/ofr_1/user_projects/domains/SBErpc12/config/config.xml > new_config.xml
 
-#mv /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml.original
-#mv new_config.xml /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml
+mv /home/oracle/Oracle/Middleware12c/ofr_1/user_projects/domains/SBErpc12/config/config.xml /home/oracle/Oracle/Middleware12c/ofr_1/user_projects/domains/SBErpc12/config/config.xml.original
+mv new_config.xml /home/oracle/Oracle/Middleware12c/ofr_1/user_projects/domains/SBErpc12/config/config.xml
