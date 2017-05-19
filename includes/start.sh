47d9df71fe781014ca5552c7a5c3c9316063ef5b
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 09:56:42 +0200

    correct file path

--- includes/start.sh
+++ includes/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./fqdn.sh
+source ./includes/fqdn.sh
 
 echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
@@ -8,7 +8,7 @@ echo " 1 - Testing FQDN"
 while true; do
   read -p "Choice: " choice
   case $choice in
-    [1]* ) getFQDN(); break;;
+    [1]* ) getFQDN; break;;
     [q]* ) echo "Exiting program."; break;;
     * ) echo "Select one of the possibilities. Try again.";
   esac
