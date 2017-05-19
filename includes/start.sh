bfcd69ebab9b580d22eac2e8778540ff7416815c
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 10:27:31 +0200

    check if fqdn has been set

--- includes/start.sh
+++ includes/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source ./includes/setVars.sh
-init; # Setting vars to empty
 
 echo "";
 
@@ -9,6 +8,10 @@ echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
 echo " 2 - Testing IP"
 echo " q - Exit"
+
+if [ -z "$fqdn" ]; then
+  echo "FQDN has not been set."
+fi
 
 while true; do
   read -p "Choice: " choice
