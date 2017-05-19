629051020834f6d38e9e713a022500221af00634
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 09:58:39 +0200

    test loop stuff..

--- includes/start.sh
+++ includes/start.sh
@@ -5,10 +5,12 @@ source ./includes/fqdn.sh
 echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
 
-while true; do
+loop = true:
+
+while loop; do
   read -p "Choice: " choice
   case $choice in
-    [1]* ) getFQDN; break;;
+    [1]* ) getFQDN; $loop = false;;
     [q]* ) echo "Exiting program."; break;;
     * ) echo "Select one of the possibilities. Try again.";
   esac
