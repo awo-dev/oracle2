9840b617f8ac8d274d0cfe035a7c2b244801c5c1
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 10:20:33 +0200

    testing promtpsfsd

--- includes/setVars.sh
+++ includes/setVars.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # This file is for retrieving all the variables from the user.
 
-function getFQDN() {
-  read -p "enter new FQDN (ex: my-domain.tld): " fqdn
-  return fqdn;
-}
-
-function getIP() {
-  read -p "Enter ip (ex: 5): 10.174.234." ip
-  return ip;
+function promptValue() {
+ read -p "$1"": " val
+ echo $val
 }
