3140b243b7961c7c683705dcf95b9ff9f01fa9f4
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 09:53:40 +0200

    Testing functions

---
+++ includes/fqdn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+function getFQDN() {
+  read -p "enter new FQDN (ex: my-domain.tld): " fqdn
+}
--- includes/start.sh
+++ includes/start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+source ./fqdn.sh
+
 echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
+
+while true; do
+  read -p "Choice: " choice
+  case $choice in
+    [1]* ) getFQDN; break;;
+    [q]* ) echo "Exiting program."; break;;
+    * ) echo "Select one of the possibilities. Try again.";
+  esac
+
+echo "FQDN = $fqdn";
+done
