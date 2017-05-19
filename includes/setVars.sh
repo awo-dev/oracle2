51612d5d230f55f126cd9ead93fcb41f6196d738
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 10:15:44 +0200

    testing

--- includes/setVars.sh
+++ includes/setVars.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 # This file is for retrieving all the variables from the user.
 
-function init() {
-  fqdn="";
-  externalIP="";
-  internalIP="";
-}
-
 function getFQDN() {
-  read -p "enter new FQDN (ex: my-domain.tld): " $fqdn
+  read -p "enter new FQDN (ex: my-domain.tld): " fqdn
+  return fqdn;
 }
 
 function getIP() {
   read -p "Enter ip (ex: 5): 10.174.234." ip
-
-  $externalIP = "10.174.234.$ip/24"
-  $internalIP = "10.0.3.$ip/24"
+  return ip;
 }
--- includes/start.sh
+++ includes/start.sh
@@ -3,16 +3,18 @@
 source ./includes/setVars.sh
 init; # Setting vars to empty
 
-echo "\n=================================\n";
+echo "";
 
 echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
+echo " 2 - Testing IP"
+echo " q - Exit"
 
 while true; do
   read -p "Choice: " choice
   case $choice in
     [1]* ) getFQDN;;
-    [2]* ) getIP;;
+    [2]* ) getIP; ip=getIP; internalIP="10.174.234.$ip/24"; externalIP="10.0.3.$ip/24";;
     [q]* ) echo "Exiting program."; break;;
     * ) echo "Select one of the possibilities. Try again.";
   esac
