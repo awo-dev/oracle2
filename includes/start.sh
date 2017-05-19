dbc6d7af9def158174aeb6c3a2b7f6aa0cb32860
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 10:31:42 +0200

    starting on network

--- includes/start.sh
+++ includes/start.sh
@@ -7,6 +7,7 @@ echo "";
 echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
 echo " 2 - Testing IP"
+echo " 3 - Setup Network"
 echo " q - Exit"
 
 while true; do
@@ -14,6 +15,7 @@ while true; do
   case $choice in
     [1]* ) getFQDN;;
     [2]* ) getIP;;
+    [2]* ) ./includes/linux/setup_network.sh;;
     [q]* ) echo "Exiting program."; break;;
     * ) echo "Select one of the possibilities. Try again.";
   esac
