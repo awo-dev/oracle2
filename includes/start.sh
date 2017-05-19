f651f017cf9f42f4c74a0d555799692dd26cb805
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 10:01:46 +0200

    done placed the wrong place...

--- includes/start.sh
+++ includes/start.sh
@@ -5,9 +5,14 @@ source ./includes/fqdn.sh
 echo "Choose one of the following possibilities:"
 echo " 1 - Testing FQDN"
 
-# Optimize with a loop asking questions later. For now, this is just fine.
-getFQDN;
+while loop; do
+  read -p "Choice: " choice
+  case $choice in
+    [1]* ) getFQDN; $loop = false;;
+    [q]* ) echo "Exiting program."; break;;
+    * ) echo "Select one of the possibilities. Try again.";
+  esac
+done
 
 echo "Program done."
 echo "FQDN = $fqdn";
-done
