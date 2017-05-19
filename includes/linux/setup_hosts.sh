17ccd10f20aa04f42223a5d418705f9fe8cdacef
Anders W. Olsen <anders@wiberg.tech>
2017-05-19 11:00:56 +0200

    wrong ip

--- includes/linux/setup_hosts.sh
+++ includes/linux/setup_hosts.sh
@@ -12,7 +12,7 @@ externalIP=$2;
 # Add FQDN to the external IP           : sed "/^#10.174.234.99/ s/$/ $1/" /etc/hosts
 # Change the external IP address        : sed "s/10.174.234.99/10.174.234.$2/" /etc/hosts
 
-sed -e "s/\S*\(changefmw\)\S*//g" -e "/^#127.0.0.1/ s/$/ $fqdn/" -e "/^#10.174.234.99/ s/$/ $fqdn/" -e "s/10.174.234.99/$externalIP/" /etc/hosts > ./new_hosts.txt
+sed -e "s/\S*\(changefmw\)\S*//g" -e "/^#127.0.0.1/ s/$/ $fqdn/" -e "/^#10.174.234.98/ s/$/ $fqdn/" -e "s/10.174.234.98/$externalIP/" /etc/hosts > ./new_hosts.txt
 #mv /etc/hosts /etc/hosts.original
 #mv ./new_hosts.txt /etc/hosts
 
